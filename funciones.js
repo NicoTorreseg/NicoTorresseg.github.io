8d04bc567373e8ad09683532bfabaeed0cb19079
NicoTorreseg <126654785+NicoTorreseg@users.noreply.github.com>
2023-03-01 04:54:39 -0300

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,26 @@
+function Mostrardatos(a){
+    
+  
+
+    const $elemento = document.querySelector("#textodatos");
+
+    switch(a){
+case 1:
+ 
+    $elemento.innerHTML = "Beverly20@gmail.com";
+    break;
+    case 2:
+        $elemento.innerHTML = "+5411233393";
+    break;
+
+    case 3:
+        $elemento.innerHTML = "Av. Rivadavia 100";
+    break;
+
+
+
+    }
+    
+    
+    
+}
